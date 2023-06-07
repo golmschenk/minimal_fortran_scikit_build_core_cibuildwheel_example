f31090e8ad8f6c873da03f3dee2c20abe23b19a6
golmschenk <greg@olmschenk.com>
2023-06-07 17:42:03 -0400

    Add minimal example
    Switch from executable to library
    
    Add sub routine
    
    Add fast fail

---
+++ main.f90
@@ -0,0 +1,10 @@
+module eesunhong_recipes_replacements
+    implicit none
+contains
+    subroutine hello_world()
+        implicit none
+        write(*,*) "Hello, World!"
+    end subroutine hello_world
+end module eesunhong_recipes_replacements
+write(*,*) "Hello, World!"
+end program
